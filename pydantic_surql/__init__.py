8a149c1d03ff1822593221852c4d7cb062efdbeb
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2024-01-05 14:01:11 +0200

    added non decorator function to generate a SurQLTable object

--- pydantic_surql/__init__.py
+++ pydantic_surql/__init__.py
@@ -4,14 +4,20 @@ from .types import SurQLTable, SurQLMapper, SurQLTableConfig
 
 Mapper = SurQLMapper(tables=[])
 
-def toSurql(name: str, config: SurQLTableConfig = SurQLTableConfig()):
+def model_to_surql(name: str, config: SurQLTableConfig, model: BaseModel) -> SurQLTable:
+    """
+        Convert a pydantic model to a SurQLTable
+    """
+    model.__is_surql_collection__ = True
+    model.__surql_table_name__ = name
+    return SurQLTable(name=name, fields=parseFields(model), config=config)
+
+def to_surql(name: str, config: SurQLTableConfig = SurQLTableConfig()):
     """
         A simple decorator to onvert a pydantic model to a surQL SDL table definition
     """
     def inner(model: BaseModel):
-        model.__is_surql_collection__ = True
-        model.__surql_table_name__ = name
-        table = SurQLTable(name=name, fields=parseFields(model), config=config)
+        table = model_to_surql(name, config, model)
         Mapper.tables.add(table)
         return model
     return inner
