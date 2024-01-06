9c236cca6d8610854bf962dd204ea1fcd14672a4
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2024-01-06 19:14:23 +0200

    pydantic_surql/types/event.py

--- pydantic_surql/types/event.py
+++ pydantic_surql/types/event.py
@@ -7,18 +7,17 @@ class SurQLEvent(BaseModel):
     """
     name: str = Field(min_length=1)
     whenSDL: list[str] = Field(default=[], description="event when definition, if list default to OR", min_length=1)
-    querySDL: list[str] = Field(default=[], min_length=1)
+    querySDL: list[str] = Field(default=[], min_length=1, description="event THEN definition, each item will be a single line in the query SDL")
 
     @model_validator('after')
     def validate_event(self):
-        assert len(self.SDL) > 0, "event SDL definition is required"
-        assert (len(self.types) > 0) or (self.whenSDL is not None), "event types or when is required"
+        assert len(self.SDL) > 0, "event %s SDL definition is required" % self.name
+        assert len(self.whenSDL) > 0, "event %s WHEN close required" % self.name
         return self
 
     def SDL(self, table_name: str):
         """
             return a SDL event definition
-            if (when)
         """
         _def = [
             "DEFINE EVENT",
