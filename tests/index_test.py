41edd902246696544a0ac276e9745090837d12e5
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2024-01-06 16:50:16 +0200

    working analyzer.filters regex

--- tests/index_test.py
+++ tests/index_test.py
@@ -28,17 +28,21 @@ class TestIndexes:
     def test_bad_analyzers_filters(self):
         try:
             analyzer = SurQLAnalyzer(name="test_analyzer", tokenizers=[SurQLTokenizers.BLANK], filters=[
-                "lowercase",
-                "ascii",
-                "uppercase",
-                "edgengram(1,2)",
-                "snowball(english)",
-                "snowball(french)",
                 "bad_filter"
             ])
             assert False, "bad filter should raise an exception"
         except Exception as e:
             pass
+
+    def test_bad_analyzers_snowball_country(self):
+        try:
+            analyzer = SurQLAnalyzer(name="test_analyzer", tokenizers=[SurQLTokenizers.BLANK], filters=[
+                "snowball(bad_country)"
+            ])
+            assert False, "bad filter should raise an exception"
+        except Exception as e:
+            pass
+
     # def test_search_index():
     #     index = SurQLSearchIndex(name="test_index", fields=["name", "age"], analyzer=SurQLAnalyzer(name="test_analyzer", tokenizers=["unicode"], filters=["lowercase"]))
     #     assert index.SDL("test_table") == "DEFINE INDEX test_index FIELDS name,age ANALYZER test_analyzer;"
